5d3f20f932da338444cbe944151d75239c8f64b5
studio02-sannomiya <sozoshastudio2@gmail.com>
2025-12-11 12:54:24 +0900

    6章まで終了

---
+++ app/news/layout.tsx
@@ -0,0 +1,17 @@
+import Hero from "../_components/Hero/Hero";
+import Sheet from "../_components/Sheet/Sheet";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function NewsLayout({ children }: Props) {
+  return (
+    <>
+      <Hero title="News" sub="ニュース" />
+      <Sheet>
+        {children}
+        </Sheet>
+    </>
+  );
+}
