f81b7f213edd2707124a1f51587e9ea74144f167
studio02-sannomiya <sozoshastudio2@gmail.com>
2025-12-18 12:36:27 +0900

    9章まで完了

---
+++ app/contact/layout.tsx
@@ -0,0 +1,17 @@
+import Hero from "../_components/Hero/Hero";
+import Sheet from "../_components/Sheet/Sheet";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: Props) {
+  return (
+    <>
+      <Hero title="Contact" sub="お問い合わせ" />
+      <Sheet>
+        {children}
+        </Sheet>
+    </>
+  );
+}
